59aa7acbb0420e74d56d9e838283489615d0da28
Leemo Tseng <LeemoTseng@tpe.thi-group.com>
2024-12-03 15:51:46 +0800

    update width of container

--- assets/main.js
+++ assets/main.js
@@ -280,7 +280,7 @@ function membersForEach(members) {
     return `
       <!-- intro start -->
       <table
-        width="600"
+        width="850"
         style="font-family: '微軟正黑體', sans-serif;padding: 0; margin: 0; margin-top: 10px"
         border="0"
         cellpadding="5"
@@ -288,13 +288,13 @@ function membersForEach(members) {
       >
         <tr>
           <td
-            width="300"
+            width="400"
             valign="top"
             style="background-color: #fff; text-align: center"
           >
             <!-- 300x320 -->
             <img
-              width="250"
+              width="450"
               height="auto"
               style="border-radius: 10px;"
               src="${imageName}"
@@ -302,11 +302,12 @@ function membersForEach(members) {
             />
           </td>
 
-          <td width="300" valign="top" style="padding: 10px; background-color: #fff";>
+          <td width="450" valign="top" style="padding: 10px; background-color: #fff";>
             <p style="padding: 0;margin: 0;font-family: '微軟正黑體', sans-serif;font-size: 14px; font-weight: 500;">自我介紹 Self-Introduction</p>
             <p style="padding: 10px 0px;margin: 0;font-family: '微軟正黑體', sans-serif;font-size: 20px; font-weight: 800;">${name}</p>
             <p style="padding: 0;margin: 0;font-family: '微軟正黑體', sans-serif;font-size: 16px;">${introduction}</p>
             <div style="border-top: 1px solid #e0e0e0; width: 100%; margin: 10px 0;"></div>
+            <p></p>
             <p style="padding: 0;margin: 0;font-family: '微軟正黑體', sans-serif;color: rgb(66, 66, 66); font-weight: 500; font-size: 14px;">報到日期 <span style="color: rgb(134, 134, 134);padding-left: 8px;">${startDate}</span></p>
             <p style="padding: 0;margin: 0;font-family: '微軟正黑體', sans-serif;color: rgb(66, 66, 66); font-weight: 500; font-size: 14px;">部門職位 <span style="color: rgb(134, 134, 134);padding-left: 8px;">${position}</span></p>
             <p style="padding: 0;margin: 0;font-family: '微軟正黑體', sans-serif;color: rgb(66, 66, 66); font-weight: 500; font-size: 14px;">直屬主管 <span style="color: rgb(134, 134, 134);padding-left: 8px;">${dirSupervisor}</span></p>
